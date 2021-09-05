4c50152f188a9e9ccce93edb8d036175d229dc78
songdabin <thdekqls1222@naver.com>
2021-09-05 18:30:17 +0900

    second commit

--- src/com/example/lab1/Main.java
+++ src/com/example/lab1/Main.java
@@ -1,9 +1,15 @@
 package com.example.lab1;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
 		System.out.println("Hello World!");
+		System.out.println("Enter name: ");
+		String name = s.next();
+		System.out.println("Your name is " + name + ".");
 	}
 
 }
